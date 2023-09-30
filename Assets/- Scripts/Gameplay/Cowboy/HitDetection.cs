2863af0dbf5d1d6cc26c01a4441858d4243309c9
banana <bongobango22@gmail.com>
2023-09-30 04:10:34 +0200

    Some basic Test

---
+++ Assets/- Scripts/Gameplay/Cowboy/HitDetection.cs
@@ -0,0 +1,20 @@
+using System;
+using UnityEngine;
+
+namespace Game
+{
+    public class HitDetection : MonoBehaviour
+    {
+        public event Action HitByBullet;
+        [SerializeField] private LayerMask activeBulletLayer;
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if ((activeBulletLayer.value & (1 << collision.transform.gameObject.layer)) > 0)
+            {
+                Debug.Log("touched active bullet", collision.gameObject);
+                HitByBullet?.Invoke();
+            }
+        }
+    }
+}
