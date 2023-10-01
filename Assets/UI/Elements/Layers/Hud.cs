50db253cf39a95ca5ffe058a6b663a75afdd0ce2
Misteeps <30960779+Misteeps@users.noreply.github.com>
2023-10-01 09:26:09 -0700

    Created ui hud star slam

--- Assets/UI/Elements/Layers/Hud.cs
+++ Assets/UI/Elements/Layers/Hud.cs
@@ -1,6 +1,7 @@
 using System;
 
 using UnityEngine;
+using StyleBackground = UnityEngine.UIElements.StyleBackground;
 
 using Simplex;
 using Simplex.UI;
@@ -31,22 +32,34 @@ namespace Game.UI
 
 			public async void Slam()
 			{
+				star.TransitionScaleX().Modify( 3, 0, EaseFunction.Circular, EaseDirection.Out).Run();
+				star.TransitionScaleY().Modify( 3, 0, EaseFunction.Circular, EaseDirection.Out).Run();
+
+				star.TransitionTranslateX().Modify(0, 60, 0.4f, EaseFunction.Circular, EaseDirection.Out).Run();
+				star.TransitionTranslateY().Modify(0, 10, 0.4f, EaseFunction.Circular, EaseDirection.Out).Run();
+				star.TransitionScaleX().Modify(1, 4, 0.4f, EaseFunction.Circular, EaseDirection.Out).Run();
+				await star.TransitionScaleY().Modify(1, 4, 0.4f, EaseFunction.Circular, EaseDirection.Out).Await();
+
+				star.TransitionTranslateX().Modify(0, 0.2f, EaseFunction.Exponential, EaseDirection.Out).Run();
+				star.TransitionTranslateY().Modify(0, 0.2f, EaseFunction.Exponential, EaseDirection.Out).Run();
+				star.TransitionScaleX().Modify(1, 0.2f, EaseFunction.Exponential, EaseDirection.Out).Run();
+				await star.TransitionScaleY().Modify(1, 0.2f, EaseFunction.Exponential, EaseDirection.Out).Await();
 			}
 			public async void Shine()
 			{
-				star.style.backgroundImage = new UnityEngine.UIElements.StyleBackground(Monolith.Refs.bountyStars[1]);
+				star.style.backgroundImage = new StyleBackground(Monolith.Refs.bountyStars[1]);
 				await Awaitable.WaitForSecondsAsync(0.1f);
-				star.style.backgroundImage = new UnityEngine.UIElements.StyleBackground(Monolith.Refs.bountyStars[2]);
+				star.style.backgroundImage = new StyleBackground(Monolith.Refs.bountyStars[2]);
 				await Awaitable.WaitForSecondsAsync(0.1f);
-				star.style.backgroundImage = new UnityEngine.UIElements.StyleBackground(Monolith.Refs.bountyStars[3]);
+				star.style.backgroundImage = new StyleBackground(Monolith.Refs.bountyStars[3]);
 				await Awaitable.WaitForSecondsAsync(0.15f);
-				star.style.backgroundImage = new UnityEngine.UIElements.StyleBackground(Monolith.Refs.bountyStars[4]);
+				star.style.backgroundImage = new StyleBackground(Monolith.Refs.bountyStars[4]);
 				await Awaitable.WaitForSecondsAsync(0.15f);
-				star.style.backgroundImage = new UnityEngine.UIElements.StyleBackground(Monolith.Refs.bountyStars[5]);
+				star.style.backgroundImage = new StyleBackground(Monolith.Refs.bountyStars[5]);
 				await Awaitable.WaitForSecondsAsync(0.2f);
-				star.style.backgroundImage = new UnityEngine.UIElements.StyleBackground(Monolith.Refs.bountyStars[6]);
+				star.style.backgroundImage = new StyleBackground(Monolith.Refs.bountyStars[6]);
 				await Awaitable.WaitForSecondsAsync(0.2f);
-				star.style.backgroundImage = new UnityEngine.UIElements.StyleBackground(Monolith.Refs.bountyStars[0]);
+				star.style.backgroundImage = new StyleBackground(Monolith.Refs.bountyStars[0]);
 			}
 		}
 		#endregion Star
@@ -55,14 +68,19 @@ namespace Game.UI
 		protected override bool DefaultFocusable => false;
 		protected override bool DefaultPickable => false;
 
-		public readonly Star[] stars;
 		public readonly Label wantedSign;
 		public readonly Label fortuneSign;
 		public readonly Label timeSign;
+		public readonly Star[] stars;
 
 
 		public Hud()
 		{
+			wantedSign = this.Attach(new Label() { Name = "wanted", Classes = "sign" });
+			fortuneSign = this.Attach(new Label() { Name = "fortune", Classes = "sign" });
+			timeSign = this.Attach(new Label() { Name = "time", Classes = "sign" });
+			timeSign.schedule.Execute(UpdateTime).Every(1000);
+
 			Div sidebar = this.Attach(new Div() { Name = "stars" });
 			stars = new Star[5];
 			stars[0] = sidebar.Attach(new Star());
@@ -71,12 +89,21 @@ namespace Game.UI
 			stars[3] = sidebar.Attach(new Star());
 			stars[4] = sidebar.Attach(new Star());
 			sidebar.schedule.Execute(ShineStars).Every(5000);
-
-			wantedSign = this.Attach(new Label() { Name = "wanted", Classes = "sign" });
-			fortuneSign = this.Attach(new Label() { Name = "fortune", Classes = "sign" });
-			timeSign = this.Attach(new Label() { Name = "time", Classes = "sign" });
-			timeSign.schedule.Execute(UpdateTime).Every(1000);
 		}
+
+		public void UpdateWanted()
+		{
+			wantedSign.text = $"$ {Monolith.bounty:N0}";
+			wantedSign.AddToClassList("pop");
+			wantedSign.schedule.Execute(() => wantedSign.RemoveFromClassList("pop")).ExecuteLater(1);
+		}
+		public void UpdateFortune()
+		{
+			fortuneSign.text = $"$ {Monolith.fortune:N0}";
+			fortuneSign.AddToClassList("pop");
+			fortuneSign.schedule.Execute(() => fortuneSign.RemoveFromClassList("pop")).ExecuteLater(1);
+		}
+		public void UpdateTime() => timeSign.text = $"{(int)(Monolith.time / 60)}:{(int)(Monolith.time % 60):00}<br>0:{(int)(Enemies.difficulty.wavesDelay - Enemies.timer):00}";
 
 		public async void ShineStars()
 		{
@@ -105,18 +132,5 @@ namespace Game.UI
 				await Awaitable.WaitForSecondsAsync(0.4f);
 			}
 		}
-		public void UpdateWanted()
-		{
-			wantedSign.text = $"$ {Monolith.bounty:N0}";
-			wantedSign.AddToClassList("pop");
-			wantedSign.schedule.Execute(() => wantedSign.RemoveFromClassList("pop")).ExecuteLater(1);
-		}
-		public void UpdateFortune()
-		{
-			fortuneSign.text = $"$ {Monolith.fortune:N0}";
-			fortuneSign.AddToClassList("pop");
-			fortuneSign.schedule.Execute(() => fortuneSign.RemoveFromClassList("pop")).ExecuteLater(1);
-		}
-		public void UpdateTime() => timeSign.text = $"{(int)(Monolith.time / 60)}:{(int)(Monolith.time % 60):00}<br>0:{(int)(Enemies.difficulty.wavesDelay - Enemies.timer):00}";
 	}
 }
