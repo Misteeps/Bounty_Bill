64737cd1eb6f5761d4f5ac59b6a53fa72790266a
Misteeps <30960779+Misteeps@users.noreply.github.com>
2023-10-01 07:01:40 -0700

    Added ui hud sign text effects

--- Assets/UI/Elements/Layers/Hud.cs
+++ Assets/UI/Elements/Layers/Hud.cs
@@ -26,19 +26,17 @@ namespace Game.UI
 			timeSign.schedule.Execute(UpdateTime).Every(1000);
 		}
 
-		public async void UpdateWanted()
+		public void UpdateWanted()
 		{
 			wantedSign.text = $"$ {Monolith.bounty:N0}";
 			wantedSign.AddToClassList("pop");
-			await Awaitable.NextFrameAsync();
-			wantedSign.RemoveFromClassList("pop");
+			wantedSign.schedule.Execute(() => wantedSign.RemoveFromClassList("pop")).ExecuteLater(1);
 		}
-		public async void UpdateFortune()
+		public void UpdateFortune()
 		{
 			fortuneSign.text = $"$ {Monolith.fortune:N0}";
 			fortuneSign.AddToClassList("pop");
-			await Awaitable.NextFrameAsync();
-			fortuneSign.RemoveFromClassList("pop");
+			fortuneSign.schedule.Execute(() => fortuneSign.RemoveFromClassList("pop")).ExecuteLater(1);
 		}
 		public void UpdateTime() => timeSign.text = $"{(int)(Monolith.time / 60)}:{(int)(Monolith.time % 60):00}<br>0:{(int)(Enemies.difficulty.wavesDelay - Enemies.timer):00}";
 	}
